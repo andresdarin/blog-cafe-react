22dda206109e756c4b64b04e43bc1104a49f7809
Andrés Darín <andres.darin05@gmail.com>
2024-08-28 00:04:47 -0300

    Section 1

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -9,17 +9,15 @@ export const LandingPage = () => {
                 </div>
             </div>
             <div className="section section1">
-                <div className="image-container">
-                    <div className="background-image"></div>
-                    <div className="overlay-image">
-                        <img src="/path/to/overlay-image.png" alt="Overlay" />
-                    </div>
+                <div className="img">
+                    <img src="src/assets/LP1.png" alt="Descripción de la imagen" />
                 </div>
-                <div className="text-container">
-                    <h2>Subtítulo</h2>
-                    <h1>Título</h1>
-                    <p>Este es el párrafo con el contenido deseado. Puedes agregar más información aquí.</p>
-                </div>
+                <div className="text">
+                    <h2>The Coffee Box</h2>
+                    <h1>Extracción de café</h1>
+                    <h3>Tecnicas de extracción de café</h3>
+                    <p>
+                        El arte de extraer café abarca diversas técnicas, cada una aportando un perfil de sabor único. Desde el espresso, que produce un café concentrado y con cuerpo, hasta el pour over, que destaca sabores delicados mediante un flujo de agua controlado, las opciones son variadas. La prensa francesa ofrece un café de cuerpo completo, mientras que la Aeropress permite ajustes personalizados en la extracción. Factores como el tamaño de la molienda, la temperatura del agua y el tiempo de extracción son esenciales para obtener un café balanceado, evitando resultados sub-extraídos o amargos.              </p>  </div>
             </div>
 
             <div className="section section2">
